05a8509eac99208f1c60d81771c56f6bbb31d376
Popova Daria <jija0877@gmail.com>
2023-10-11 11:15:52 +0300

    Added a function that allows you to sort the stack in ascending order

--- src/Main_lab2.java
+++ src/Main_lab2.java
@@ -1,14 +1,33 @@
+//
+//public class Main_lab2 {
+//	public static void main (String args[]){
+//		MyStack<Integer> stack = new MyStack<>();
+//		MyStack<Integer> NewStack = new MyStack<>();
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//        stack.push(14);
+//        stack.push(5);
+//        stack.printStack();
+//	}
+//
+//}
 
 public class Main_lab2 {
-	public static void main (String args[]){
-		MyStack<Integer> stack = new MyStack<>();
-		MyStack<Integer> NewStack = new MyStack<>();
+    public static void main(String[] args) {
+        MyStack<Integer> stack = new MyStack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
         stack.push(14);
         stack.push(5);
-        stack.printStack();
-	}
 
+        System.out.println("Исходный стек:");
+        stack.printStack();
+
+        stack.sortAscending();
+
+        System.out.println("Отсортированный стек в возрастающем порядке:");
+        stack.printStack();
+    }
 }
