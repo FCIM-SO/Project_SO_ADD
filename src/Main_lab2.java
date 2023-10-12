a0438e9dbfa46de80451bcf4130c5846a77e4473
Igor Franciuc <ifeerus@gmail.com>
2023-10-12 08:49:13 +0300

    Creating sort menu

--- src/Main_lab2.java
+++ src/Main_lab2.java
@@ -20,9 +20,24 @@ public class Main_lab2 {
         System.out.println("Исходный стек:");
         stack.printStack();
 
-        stack.sortAscending();
+        System.out.println("Меню:");
+        System.out.println("1. Сортировать в возрастающем порядке");
+        System.out.println("2. Сортировать в убывающем порядке");
 
-        System.out.println("Отсортированный стек в возрастающем порядке:");
-        stack.printStack();
+        System.out.print("Выберите опцию: ");
+        int option = scanner.nextInt();
+
+        if (option == 1) {
+            System.out.println("Выбрана сортировка в возрастающем порядке.");
+            stack.sortAscending();
+            stack.printStack();
+        } else if (option == 2) {
+            System.out.println("Выбрана сортировка в убывающем порядке.");
+            stack.sortDescending();
+            stack.printStack();
+        } else {
+            System.out.println("Неверная опция. Выбрана сортировка в возрастающем порядке по умолчанию.");
+            stack.sortAscending();
+        }
     }
 }
--- src/MyStack.java
+++ src/MyStack.java
@@ -1,75 +1,3 @@
-//public class MyStack<T> {
-//    private static final int DEFAULT_CAPACITY = 10;
-//    private Object[] array;
-//    private int size;
-//
-//    public MyStack() {
-//        array = new Object[DEFAULT_CAPACITY];
-//        size = 0;
-//    }
-//    private void swap(int index1, int index2) {
-//        Object temp = array[index1];
-//        array[index1] = array[index2];
-//        array[index2] = temp;
-//    }
-//
-//    public void printStack() {
-//        System.out.print("Стек: ");
-//        for (int i = 0; i < size; i++) {
-//            System.out.print(array[i]);
-//            if (i < size - 1) {
-//                System.out.print(" -> ");
-//            }
-//        }
-//        System.out.println();
-//    }
-//    // Вставка элемента в стек
-//    public void push(T element) {
-//        if (size == array.length) {
-//            // Увеличиваем размер массива при необходимости
-//            resizeArray();
-//        }
-//        array[size++] = element;
-//    }
-//
-//    // Удаление элемента из стека
-//    public T pop() {
-//        if (isEmpty()) {
-//            throw new IllegalStateException("Стек пуст");
-//        }
-//        T element = peek();
-//        array[--size] = null; // Удаляем ссылку на элемент
-//        return element;
-//    }
-//
-//    // Возвращает верхний элемент стека
-//    public T peek() {
-//        if (isEmpty()) {
-//            throw new IllegalStateException("Стек пуст");
-//        }
-//        return (T) array[size - 1];
-//    }
-//
-//    // Возвращает true, если стек пуст, иначе false
-//    public boolean isEmpty() {
-//        return size == 0;
-//    }
-//
-//    // Возвращает размер стека
-//    public int size() {
-//        return size;
-//    }
-//
-//    // Увеличивает размер массива
-//    private void resizeArray() {
-//        int newCapacity = array.length * 2;
-//        Object[] newArray = new Object[newCapacity];
-//        System.arraycopy(array, 0, newArray, 0, size);
-//        array = newArray;
-//    }
-//
-//    }
-//
 import java.util.Stack;
 
 public class MyStack<T extends Comparable<T>> {
@@ -119,6 +47,26 @@ public class MyStack<T extends Comparable<T>> {
         while (!tempStack.isEmpty()) {
             stack.push(tempStack.pop());
         }
+    }
+
+    public void sortDescending() {
+        Stack<T> tempStack = new Stack<>();
+
+        while (!stack.isEmpty()) {
+            T current = stack.pop();
+            while (!tempStack.isEmpty() && current.compareTo(tempStack.peek()) < 0) {
+                stack.push(tempStack.pop());
+            }
+            tempStack.push(current);
+        }
+
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
+    }
+
+    public boolean contains(T element) {
+        return stack.contains(element);
     }
 
     public void printStack() {
