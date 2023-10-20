bf40aab34bfbe99335b24ee040fada63895a9613
Дарья Чубара <kimdara985@gmail.com>
2023-10-20 15:44:09 +0300

    Adding method use() in Warehouse.java

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -11,4 +11,16 @@ public class Warehouse {
         products.add(product);
         System.out.println("Добавлен товар: " + product);
     }
+
+    // Метод для потребления товара из склада
+    public void use() {
+        if (!products.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(products.size());
+            String product = products.get(randomIndex);
+            products.remove(randomIndex);
+            System.out.println("Потреблен товар: " + product);
+        } else {
+            System.out.println("Склад пуст, нет товаров для потребления.");
+        }
 }
