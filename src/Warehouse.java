7de96ee66ec3e61458e88ffd0f6a5a44dc0db07d
Igor Franciuc <ifeerus@gmail.com>
2023-10-20 15:53:44 +0300

    Adding create_bread() method in Warehouse.java

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -4,7 +4,16 @@ public class Warehouse {
     private ArrayList<String> products = new ArrayList<>();
 
     public void create_milk() {
-        String[] possibleProducts = {"Молоко","Сыр"};
+        String[] possibleProducts = {"Молоко", "Сыр"};
+        Random random = new Random();
+        int randomIndex = random.nextInt(possibleProducts.length);
+        String product = possibleProducts[randomIndex];
+        products.add(product);
+        System.out.println("Добавлен товар: " + product);
+    }
+
+    public void create_bread() {
+        String[] possibleProducts = {"Хлеб", "Булка", "Пирожок", "Булочка"};
         Random random = new Random();
         int randomIndex = random.nextInt(possibleProducts.length);
         String product = possibleProducts[randomIndex];
@@ -23,4 +32,5 @@ public class Warehouse {
         } else {
             System.out.println("Склад пуст, нет товаров для потребления.");
         }
+    }
 }
