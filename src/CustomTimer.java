6a2d201d9d11ceceb20660bbbd47c7558c20d428
Popova Daria <jija0877@gmail.com>
2023-10-26 21:40:45 +0300

    Changing methods

--- src/CustomTimer.java
+++ src/CustomTimer.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CustomTimer {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomTimer {
+    private List<TimerTask> tasks = new ArrayList<>();
+
+    public void schedule(TimerTask task, long delay) {
+        task.schedule(delay);
+        tasks.add(task);
+    }
+
+    public void cancel(TimerTask task) {
+        task.cancel();
+        tasks.remove(task);
+    }
+
+    public void run() {
+        long currentTime = System.currentTimeMillis();
+        for (TimerTask task : new ArrayList<>(tasks)) {
+            task.run();
+        }
+    }
 }
--- src/TimerTask.java
+++ src/TimerTask.java
@@ -1,9 +1,34 @@
-
 public class TimerTask {
+	private Runnable runnable;
+	private long delay;
+	private boolean isScheduled = false;
+	private long executionTime;
 
-	public TimerTask(Object object) {
-		// TODO Auto-generated constructor stub
+	public TimerTask(Runnable runnable) {
+		this.runnable = runnable;
 	}
 
-	
+	public void schedule(long delay) {
+		this.delay = delay;
+		this.executionTime = System.currentTimeMillis() + delay;
+		this.isScheduled = true;
+	}
+
+	public void cancel() {
+		this.isScheduled = false;
+	}
+
+	public boolean isScheduled() {
+		return isScheduled;
+	}
+
+	public void run() {
+		if (isScheduled) {
+			long currentTime = System.currentTimeMillis();
+			if (currentTime >= executionTime) {
+				runnable.run();
+				isScheduled = false;
+			}
+		}
+	}
 }
