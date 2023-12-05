8f653639d68cf08717c377423061b47679b3df13
Дарья Чубара <kimdara985@gmail.com>
2023-12-05 22:17:16 +0200

    Change class Reader

--- src/Reader.java
+++ src/Reader.java
@@ -1,3 +1,30 @@
-public class Reader {
+
+// Reader.java
+public class Reader implements Runnable {
     private static final int BOOKS_TO_READ = 9;
+    private final Library library;
+
+    public Reader(Library library) {
+        this.library = library;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < BOOKS_TO_READ; i++) {
+            readBook();
+        }
+    }
+
+    private void readBook() {
+        Book adventureBook = new Book("Adventure");
+        System.out.println(Thread.currentThread().getName() + " is reading an " + adventureBook.getGenre() + " book.");
+        // Simulating the process of reading a book
+        try {
+            Thread.sleep(50); // Reading takes less time than writing
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        library.borrowBook(adventureBook); // Reader borrows a book from the library
+    }
 }
