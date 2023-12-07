0d00c45c7238377481343dc6bad87be09399b07e
Альберт <albert_bas@list.ru>
2023-12-07 13:37:28 +0200

    main lab 7

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.IOException;
+
+public class Main {    
+   public static void main(String[] args) throws IOException {
+          long startTime = System.currentTimeMillis();
+          Warehouse warehouse = new Warehouse();
+          Random random = new Random();
+          boolean inputProcessed = false;
+          System.out.println("Нажмите или введите любой символ для");
+
+          while (true) {
+              if (System.in.available() > 0) {
+                  int input = (int) System.in.read();
+                  if (!inputProcessed) {
+                      warehouse.use();
+                      inputProcessed = true;
+                  }
+                  } else {
+                  long currentTime = System.currentTimeMillis();
+                  if (currentTime - startTime >= 3000) {
+                    inputProcessed = false;
+                      warehouse.create_bread();
+                      int randomValue1 = random.nextInt(3); //шанс 1/3 то другой потребитель потребит, можно их добавить несколько
+                      if (randomValue1 == 0)
+                      {
+                      warehouse.use();
+                      }
+                      int randomValue2 = random.nextInt(4); //шанс 1/4 то другой потребитель потребит, можно их добавить несколько
+                      if (randomValue2 == 0)
+                      {
+                      warehouse.use();
+                      }
+                             
+                       startTime = currentTime;
+                  }}}}}
