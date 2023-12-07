733bb39141463e992aa81ac6e0eaf61ef3967291
Igor Franciuc <ifeerus@gmail.com>
2023-12-07 09:43:15 +0200

    Part for individual work

--- neural_network.py
+++ neural_network.py
@@ -126,4 +126,87 @@ class Network(object):
             biases.append(delta.reshape(-1))
         return [grades,biases]#возвращаем список весов для корректировки
 #тренируем нашу сеть
-    
+
+    photo_analyzer = joblib.load('photo_analyzer.joblib')
+
+    my_str = '0 1 2 3 4 5 6 7 8 9'
+    canvas = np.zeros((28, 28))
+    pygame.init()
+    clock = pygame.time.Clock()
+    FPS = 60
+    screen = pygame.display.set_mode((252, 252))
+    pygame.display.set_caption('Рисовалка')
+
+    WHITE = (255, 255, 255)
+    x, y = 0, 0
+    line_thickness = 21
+    running = True
+    pygame.init()
+
+    # Set up the display
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Drawing and Output String")
+
+    # Colors
+    WHITE = (255, 255, 255)
+
+    # Set up the canvas
+    canvas = np.zeros((252, 252))
+
+    # Set up the drawing variables
+    line_thickness = 10
+    x, y = 0, 0
+    running = True
+
+    # Set up the clock
+    clock = pygame.time.Clock()
+    FPS = 15
+
+    # Set up the font
+    font = pygame.font.Font(None, 36)
+    font1 = pygame.font.Font(None, 52)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pygame.draw.circle(screen, WHITE, event.pos, line_thickness // 2)
+                    x, y = event.pos
+                    canvas[y // 10, x // 10] = 1
+                elif event.type == pygame.MOUSEMOTION:
+                    if pygame.mouse.get_pressed()[0]:
+                        pygame.draw.line(screen, WHITE, (x, y), event.pos, line_thickness)
+                    x, y = event.pos
+                    canvas[y // 10, x // 10] = 1
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_d:
+                        screen.fill((0, 0, 0))
+                    canvas = np.zeros((252, 252))
+
+    pygame.image.save(screen, "Test_photo.png")
+    img = Image.open('Test_photo.png')
+    new_img = img.resize((28, 28))
+    new_img.save('Test_photo.png', 'PNG')
+    image = Image.open('Test_photo.png').convert('L')
+    img_array = np.array(image)
+    pixels = img_array.reshape(-1) / 255
+    # Assuming photo_analyzer is defined somewhere in your code
+    a = photo_analyzer.feedforward(pixels)
+    summa = np.sum(a)
+    percent = a / summa * 100
+    output_string = f"{round(percent[0], 2)}% {round(percent[1], 2)}% {round(percent[2], 2)}% {round(percent[3], 2)}% {round(percent[4], 2)}% {round(percent[5], 2)}% {round(percent[6], 2)}% {round(percent[7], 2)}% {round(percent[8], 2)}% {round(percent[9], 2)}%"
+    # Create a surface to render the text
+    text_surface = font.render(output_string, True, WHITE)
+    text_surface1 = font1.render(my_str, True, WHITE)
+
+    # Blit the text surface onto the screen
+    screen.fill((0, 0, 0), (0, 0, screen.get_width(), 50))
+    screen.blit(text_surface, (10, 10))
+    screen.blit(text_surface1, (10, 50))
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+    pygame.quit()
